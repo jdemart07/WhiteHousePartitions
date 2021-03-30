3a4123c25747f94598f7b17352e6e2033b641d8f
Justin De Martinis <51729801+jdemart07@users.noreply.github.com>
2021-03-29 21:07:21 -0400

    Add files via upload

---
+++ WhitehousePartitions/WhitehousePartitions/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  WhitehousePartitions
+//
+//  Created by Justin DeMartinis on 3/24/21.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ WhitehousePartitions/WhitehousePartitions/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  WhitehousePartitions
+//
+//  Created by Justin DeMartinis on 3/24/21.
+//
+
+import Foundation
+
+struct Petitions: Codable{
+    var results: [Petition]
+}
